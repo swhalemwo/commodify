dd0874fbc16ba1fcfff14624b31b138d1a78dc96
swhalemwo <jojo.ae@hotmail.de>
2024-08-23 10:15:33 +0200

    update README and example

--- commodify_example.R
+++ commodify_example.R
@@ -4,10 +4,12 @@ FILE_EOD_PRICEHIST <- "/home/link/to/prices.db"
 
 ## defining stocks to track
 ## ISINs are not needed for EODHD, but I added them anyways (should not be necessary)
-dt_stock_ids <- list(
+l_stock_ids <- list(
     list(isin = "LU1681048804", ticker = "AUM5", name = "SP500"),
     list(isin = "DE000A0H08J9", ticker = "EXH4", name = "STX600")
-) %>% rbindlist
+)
+
+df_stock_ids <- do.call(rbind, lapply(l_stock_ids, as.data.frame))
 
 source("/path/to/commodify_funcs.R")
-gd_eod(dt_stock_ids)
+gd_eod(df_stock_ids)
